f3ac49d895e0f1d67f22c2bc45bba0972ec8da1f
Luke Phyall <lukeprospect@yahoo.com>
2019-02-06 11:09:30 +0000

    First test works and passes. Syntax a little long winded.

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -1,7 +1,14 @@
+require './lib/exercise_3.rb'
+
 describe Library do
   it 'can return a list of all books' do
     library = Library.new
-    expect(library.list_books). to include(@books)
+    expect(library.list_books).to eq [
+      {title: 'POODR', author: 'Sandi Metz', subject: 'OOP'},
+      {title: 'Learn Ruby The Hard Way', author: 'Zed Shaw', subject: 'Ruby'},
+      {title: 'Eloquent JavaScript', author: 'Marijn Haverbeke', subject: 'JS'},
+      {title: 'The Well Grounded Rubyist', author: 'Sandi Metz', subject: 'Ruby'},
+    ]
   end
 
   it 'can add a new book' do
